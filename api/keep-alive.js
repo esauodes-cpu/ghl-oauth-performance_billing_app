1cd32fa630e63365d43050d508d7ab78269d14a5
esauodes-cpu <esauodes@accelerecom.com>
2026-02-24 15:26:43 -0600

    Create keep-alive.js

---
+++ api/keep-alive.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient(process.env.DATABASE_URL, process.env.PASSWORD);
+
+export default async function handler(req, res) {
+  const { data } = await supabase.from('auth_tokens').select('count');
+  res.status(200).send("Database is awake");
+}
