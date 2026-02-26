bd49222121333394a3c901da15357f160d515bbc
esauodes-cpu <esauodes@accelerecom.com>
2026-02-26 17:30:30 -0600

    Update supabase.js

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,2 +1,4 @@
+//Crea una instancia dentro de la base de datos para no tener múltiples requests a la base de datos
+// revisado : ok
 import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(process.env.DATABASE_URL, process.env.DATABASE_SERVICE_KEY);
