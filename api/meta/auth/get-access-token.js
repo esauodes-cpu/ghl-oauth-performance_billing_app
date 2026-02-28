dcec59f1a9cc232dabdc49d15063ccd70d27466e
esauodes-cpu <esauodes@accelerecom.com>
2026-02-28 11:33:21 -0600

    Update get-access-token.js

--- api/meta/auth/get-access-token.js
+++ api/meta/auth/get-access-token.js
@@ -1,9 +1,8 @@
 // api/meta/auth/get-access-token.js
-
-export const getToken = async () => {
-  const META_SYSTEM_USER_TOKEN = process.env.META_SYSTEM_USER_TOKEN;
-  if (!META_SYSTEM_USER_TOKEN) {
-    throw new Error('Missing META_SYSTEM_USER_TOKEN environment variable');
+export async function getAccessToken() {
+  if (!process.env.META_SYSTEM_USER_TOKEN) {
+    throw new Error('[Meta] Missing system user token');
   }
-  return META_SYSTEM_USER_TOKEN;
-};
+
+  return process.env.META_SYSTEM_USER_TOKEN;
+}
